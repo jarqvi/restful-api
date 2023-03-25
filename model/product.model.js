32d894238dea2023a1a7c08a26e3cfdbb05f9612
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-03-25 23:34:44 +0430

    Add endpoint for creating products

--- model/product.model.js
+++ model/product.model.js
@@ -1,22 +1,37 @@
 const products = require('../db/products.json');
+const fs = require('fs');
 
 
 async function find() {
     return new Promise((resolve, reject) => {
         resolve(products);
-    })
+    });
 }
 
 async function findById(id) {
     return new Promise((resolve, reject) => {
         resolve(products.find(product => product.id == id));
-    })
+    });
+}
+
+async function create(product) {
+    return new Promise((resolve, reject) => {
+        products.push(product);
+        fs.writeFile(`${process.cwd()}/db/products.json`, JSON.stringify(products), err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({ message: 'New product created.', data: product });
+            }
+        });
+    });
 }
 
 
 const ProductModel = {
     find,
-    findById
+    findById,
+    create
 };
 
 module.exports = ProductModel;
